0f1d89136ac1d0ff13677a619165a6336ec352c6
Chris Tankersley <chris@ctankersley.com>
2021-05-04 16:53:28 -0400

    Aligned client JWT with Arun's demo

--- src/Client.php
+++ src/Client.php
@@ -7,6 +7,7 @@ use Vonage\Client\APIClient;
 use Nexmo\JWT\TokenGenerator;
 use Vonage\Client\APIResource;
 use Vonage\Client\Credentials\Keypair;
+use Vonage\Client\Credentials\Container;
 use Vonage\Client\Credentials\CredentialsInterface;
 
 class Client implements APIClient
@@ -52,25 +53,13 @@ class Client implements APIClient
         return $session;
     }
 
-    public function generateClientToken(string $sessionId, array $options = []): string
+    public function generateClientToken(string $sessionId): string
     {
-        // unpack optional arguments (merging with default values) into named variables
-        $defaults = array(
-            'role' => Role::PUBLISHER,
-            'expireTime' => null,
-            'data' => null,
-            'initialLayoutClassList' => [],
-        );
-        $options = array_merge($defaults, array_intersect_key($options, $defaults));
-        list($role, $expireTime, $data, $initialLayoutClassList) = array_values($options);
-
         /** @var Keypair $credentials */
         $credentials = $this->extractCredentials(Keypair::class, $this->getAPIResource()->getClient()->getCredentials());
         $token = TokenGenerator::factory($credentials->application, $credentials->key, [
-            'role' => $role,
-            'connection_data' => $data,
-            'initial_layout_class_list' => $initialLayoutClassList,
-            'session_id' => $sessionId
+            'scope' => 'session.connect',
+            'session_id' => $sessionId,
         ]);
 
         return $token;
