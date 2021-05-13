5703da5ef7663a5b6519882f67493415bad4747f
Chris Tankersley <chris@ctankersley.com>
2021-05-13 13:33:14 -0400

    Basic archiving response

--- src/Client.php
+++ src/Client.php
@@ -3,12 +3,14 @@ declare(strict_types=1);
 
 namespace Vonage\Video;
 
+use Psr\Http\Message\ResponseInterface;
 use Vonage\Client\APIClient;
 use Vonage\JWT\TokenGenerator;
 use Vonage\Client\APIResource;
 use Vonage\Client\Credentials\Keypair;
 use Vonage\Client\Credentials\Container;
 use Vonage\Client\Credentials\CredentialsInterface;
+use Vonage\Video\ArchiveTest\ArchiveObject;
 
 class Client implements APIClient
 {
@@ -54,18 +56,6 @@ class Client implements APIClient
         return $session;
     }
 
-    public function generateClientToken(string $sessionId): string
-    {
-        /** @var Keypair $credentials */
-        $credentials = $this->extractCredentials(Keypair::class, $this->getAPIResource()->getClient()->getCredentials());
-        $token = TokenGenerator::factory($credentials->application, $credentials->key, [
-            'scope' => 'session.connect',
-            'session_id' => $sessionId,
-        ]);
-
-        return $token;
-    }
-
     protected function extractCredentials(string $class, CredentialsInterface $credentials): CredentialsInterface
     {
         if ($credentials instanceof $class) {
@@ -80,5 +70,43 @@ class Client implements APIClient
         }
 
         throw new \RuntimeException('Requested auth type not found');
+    }
+
+    public function generateClientToken(string $sessionId): string
+    {
+        /** @var Keypair $credentials */
+        $credentials = $this->extractCredentials(Keypair::class, $this->getAPIResource()->getClient()->getCredentials());
+        $token = TokenGenerator::factory($credentials->application, $credentials->key, [
+            'scope' => 'session.connect',
+            'session_id' => $sessionId,
+        ]);
+
+        return $token;
+    }
+
+    public function sendSignal(string $sessionId, string $type, $data, string $connectionId = null): void
+    {
+        $credentials = $this->extractCredentials(Keypair::class, $this->getAPIResource()->getClient()->getCredentials());
+        $url = '/v2/project/' . $credentials->application . '/session/' . $sessionId . '/signal';
+        if ($connectionId) {
+            $url = '/v2/project/' . $credentials->application . '/session/' . $sessionId . '/connection/' . $connectionId . '/signal';
+        }
+
+        $this->apiResource->create(
+            ['type' => $type, 'data' => $data],
+            $url
+        );
+    }
+
+    public function startArchive($sessionId, $data)
+    {
+        $credentials = $this->extractCredentials(Keypair::class, $this->getAPIResource()->getClient()->getCredentials());
+        $data['sessionId'] = $sessionId;
+        $response = $this->apiResource->create(
+            $data,
+            '/v2/project/' . $credentials->application . '/archive'
+        );
+
+        return $response;
     }
 }
--- test/Video/ClientTest.php
+++ test/Video/ClientTest.php
@@ -11,6 +11,7 @@ use Vonage\Client as VonageClient;
 use VonageTest\Psr7AssertionTrait;
 use Prophecy\PhpUnit\ProphecyTrait;
 use Psr\Http\Message\RequestInterface;
+use Vonage\Client\Credentials\Keypair;
 use Vonage\Video\ArchiveMode;
 use Vonage\Video\MediaMode;
 
@@ -25,9 +26,21 @@ class ClientTest extends TestCase
     protected $apiResource;
 
     /**
+     * Sample Application ID to use in tests
+     * @var string
+     */
+    protected $applicationId = 'd5e57267-1bd2-4d76-aa53-c1c1542efc14';
+
+    /**
      * @var Client
      */
     protected $client;
+
+    /**
+     * Sample Session ID to use in tests
+     * @var string
+     */
+    protected $sessionId = '2_999999999999999-MTYxODg4MTU5NjY3N35QY1VEUUl4MVhldEdKU2JCOWlyR2lHY3p-UH4';
 
     /**
      * @var VonageClient
@@ -39,10 +52,11 @@ class ClientTest extends TestCase
         $this->vonageClient = $this->prophesize(VonageClient::class);
         $this->vonageClient->getRestUrl()->willReturn('https://rest.nexmo.com');
         $this->vonageClient->getApiUrl()->willReturn('https://api.nexmo.com');
+        $this->vonageClient->getCredentials()->willReturn(new Keypair('abcd', $this->applicationId));
 
         $this->apiResource = new APIResource();
         $this->apiResource
-            ->setBaseUri('/video')
+            ->setBaseUri('/')
             ->setClient($this->vonageClient->reveal())
         ;
 
@@ -57,13 +71,46 @@ class ClientTest extends TestCase
 
         $session = $this->client->createSession();
 
-        $this->assertSame('2_999999999999999-MTYxODg4MTU5NjY3N35QY1VEUUl4MVhldEdKU2JCOWlyR2lHY3p-UH4', $session->getSessionId());
+        $this->assertSame($this->sessionId, $session->getSessionId());
         $this->assertSame(null, $session->getLocation());
         $this->assertSame(MediaMode::RELAYED, $session->getMediaMode());
         $this->assertSame(ArchiveMode::MANUAL, $session->getArchiveMode());
         $this->assertSame('99999999', $session->getProjectId());
         $this->assertEquals(new \DateTimeImmutable('2021-01-01 00:00:00'), $session->getCreatedDate());
         $this->assertSame('10.10.10.10', $session->getMediaServerUrl());
+    }
+
+    public function testCanSendSignalToEveryoneInSession()
+    {
+        $applicationId = $this->applicationId;
+        $sessionId = $this->sessionId;
+
+        $this->vonageClient->send(Argument::that(function (RequestInterface $request) use ($applicationId, $sessionId) {
+            $this->assertRequestJsonBodyContains('type', 'car', $request);
+            $this->assertRequestJsonBodyContains('data', 'sedan', $request);
+            $this->assertSame('/v2/project/' . $applicationId . '/session/' . $sessionId . '/signal', $request->getUri()->getPath());
+
+            return true;
+        }))->shouldBeCalledTimes(1)->willReturn($this->getResponse('empty'));
+
+        $this->client->sendSignal($sessionId, 'car', 'sedan');
+    }
+
+    public function testCanSendSignalToSingleConnectionInSession()
+    {
+        $applicationId = $this->applicationId;
+        $sessionId = $this->sessionId;
+        $connectionId = 'iqu34ruqi';
+
+        $this->vonageClient->send(Argument::that(function (RequestInterface $request) use ($applicationId, $sessionId, $connectionId) {
+            $this->assertRequestJsonBodyContains('type', 'car', $request);
+            $this->assertRequestJsonBodyContains('data', 'sedan', $request);
+            $this->assertSame('/v2/project/' . $applicationId . '/session/' . $sessionId . '/connection/' . $connectionId . '/signal', $request->getUri()->getPath());
+
+            return true;
+        }))->shouldBeCalledTimes(1)->willReturn($this->getResponse('empty'));
+
+        $this->client->sendSignal($sessionId, 'car', 'sedan', $connectionId);
     }
 
     protected function getResponse(string $type = 'success', int $status = 200): Response
